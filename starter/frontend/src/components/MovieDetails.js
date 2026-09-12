d34d77a3608853fe520d883e065400c734c1e758
Nehay015 <nehay4229@gmail.com>
2026-09-12 21:17:26 +0530

    Fixed backend URLs in both components

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function MovieDetail({ movie }) {
   const [details, setDetails] = useState(null);
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`).then((response) => {
+    axios.get(`http://a763b03932b31428c9d9ce354118feda-2091843276.us-east-1.elb.amazonaws.com/movies/${movie.id}`).then((response) => {
       setDetails(response.data);
     });
   }, [movie]);
--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,7 +6,7 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
+    axios.get('http://a763b03932b31428c9d9ce354118feda-2091843276.us-east-1.elb.amazonaws.com/movies').then((response) => {
       setMovies(response.data.movies);
     });
   }, []);
